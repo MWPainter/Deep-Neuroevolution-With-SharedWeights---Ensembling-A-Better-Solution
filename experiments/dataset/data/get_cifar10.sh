0bde0be8a42bb579950865b537e9786a9927fd09
MWPainter <michaelpainter.1994@gmail.com>
2018-10-14 15:13:07 +0100

    Actually deleted experiments folder and dataset folder this time. Move corrections to initializations to tutorial.ipynb

--- experiments/dataset/data/get_cifar10.sh
+++
@@ -1,7 +0,0 @@
-# Get CIFAR10
-wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xzvf cifar-10-python.tar.gz
-rm cifar-10-python.tar.gz 
-mkdir cifar10
-mv cifar-10-batches-py/* cifar10
-rm -r cifar-10-batches-py
