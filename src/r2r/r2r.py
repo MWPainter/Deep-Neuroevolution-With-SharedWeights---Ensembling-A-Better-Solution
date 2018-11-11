Fixed indexing wrong dimension for extending input to a linear layer's weight matrix during widen input portion of R2WiderR

src/r2r/r2r.py
@@ -360,7 +360,7 @@ def _widen_input_channels_(next_layer, extra_channels, init_type, volume_slice_i
             if scaled:
                 extra_params_per_input_layer = (end-beg) * (extra_channels - 1) # triple num outputs = *add* 2x the current num
             matrix_extra_shape = (n_out, extra_params_per_input_layer)
-            matrix_part = _extend_matrix_with_repeated_in_weights(matrix_extra_shape, next_matrix[beg:end], init_type, alpha)
+            matrix_part = _extend_matrix_with_repeated_in_weights(matrix_extra_shape, next_matrix[:,beg:end], init_type, alpha)
             next_matrix_parts.append(matrix_part)
         next_matrix = np.concatenate(next_matrix_parts, axis=1)        
         