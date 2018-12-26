1f9b6a6db6deedfa36574e0ab62fbea1d515faea
Michael Painter <michael@Administrators-MacBook-Pro.local>
2018-12-26 05:09:35 +0000

    Changed some params

--- src/main.py
+++ src/main.py
@@ -315,7 +315,7 @@ def get_defaults(script_name):
             "tb_dir": tb_log_dir,
             "checkpoint_dir": checkpoint_dir,
             "exp": exp_id,
-            "batch_size": 16,
+            "batch_size": 256,
             "workers": 6,
             "widen_times": [], # unused
             "deepen_times": [], # unused
