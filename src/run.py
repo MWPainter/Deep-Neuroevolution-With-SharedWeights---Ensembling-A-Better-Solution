efb82e321946afc3040c87a3f5eedd77eb07a7bd
Michael Painter <michael@Administrators-MacBook-Pro.local>
2018-12-26 04:23:23 +0000

    Debugging

--- src/run.py
+++ src/run.py
@@ -127,9 +127,6 @@ def _update_op(model, optimizer, minibatch, iter, args):
     :return: An updated reference to the model and optimizer, and a dictionary from strings to PyTorch scalar Variables
             used for TensorBoard summaries.
     """
-    print(minibatch)
-    raise Exception("Testing, remove this after hit it.")
-
     # If we have expended the number of flops for this test, then we should stop any updates
     if hasattr(args, "total_flops") and hasattr(args, "flops_budget") and args.total_flops >= args.flops_budget:
         return model, optimizer, {}
@@ -237,6 +234,8 @@ def _validation_loss(model, minibatch, args):
     :return: A PyTorch scalar Variable with value of the validation loss.
         Returns validation loss and validation accuracy.
     """
+    print(minibatch)
+    raise Exception("Testing, remove this after hit it.")
     # If we have expended the number of flops for this test, then we should stop any updates
     if hasattr(args, "total_flops") and hasattr(args, "flops_budget") and args.total_flops >= args.flops_budget:
         return {}
