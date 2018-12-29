09c812473eaf1141cafd1be92f75c2acc4e11491
Michael Painter <michael@Administrators-MacBook-Pro.local>
2018-12-29 14:43:12 +0000

    Actually made the batch size test run on the gpu...

--- src/batch_size_test.py
+++ src/batch_size_test.py
@@ -5,10 +5,10 @@ from r2r import resnet50
 
 
 if __name__ == "__main__":
-    model = resnet50()
+    model = resnet50().cuda()
     batch_size = 1
     while True:
-        rand_in = t.Tensor(np.random.uniform(low=-0.5, high=0.5, size=(batch_size,3,224,224)))
+        rand_in = t.Tensor(np.random.uniform(low=-0.5, high=0.5, size=(batch_size,3,224,224))).cuda()
         rand_out = model(rand_in)
         print("Can run batch size of {bs}.".format(bs=batch_size))
         batch_size = batch_size ** 2
