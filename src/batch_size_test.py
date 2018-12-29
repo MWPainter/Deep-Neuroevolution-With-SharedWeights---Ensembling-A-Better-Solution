56022a13c3b9b4114414bb32daeef3e650c66ac4
Michael Painter <michael@Administrators-MacBook-Pro.local>
2018-12-29 14:42:31 +0000

    Added a test for working out the max batchsize for a model that can be on the gpu

---
+++ src/batch_size_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch as t
+
+from r2r import resnet50
+
+
+if __name__ == "__main__":
+    model = resnet50()
+    batch_size = 1
+    while True:
+        rand_in = t.Tensor(np.random.uniform(low=-0.5, high=0.5, size=(batch_size,3,224,224)))
+        rand_out = model(rand_in)
+        print("Can run batch size of {bs}.".format(bs=batch_size))
+        batch_size = batch_size ** 2
