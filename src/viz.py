d268378ea739e9071cd40742447ace4eb67e07ac
Michael Painter <michael@Administrators-MacBook-Pro.local>
2019-01-12 02:44:25 +0000

    Fixed widening in viz

--- src/viz.py
+++ src/viz.py
@@ -148,13 +148,13 @@ class Conv_Net(nn.Module):
 
     def widen(self, num_channels=8):
         if self.widen_method == 'r2r':
-            r_2_wider_r_(self.conv1, (self.conv1.weight.data.size(0),32,32), self.W1, extra_channels=num_channels,
+            r_2_wider_r_(self.conv1, (self.conv1.weight.data.size(0),16,16), self.W1, extra_channels=num_channels,
                          init_type="match_std_exact", function_preserving=True, multiplicative_widen=self.multiplicative_widen)
         elif self.widen_method == 'net2net':
-            net_2_wider_net_(self.conv1, self.W1, (self.conv1.weight.data.size(0),32,32), extra_channels=num_channels,
+            net_2_wider_net_(self.conv1, self.W1, (self.conv1.weight.data.size(0),16,16), extra_channels=num_channels,
                              multiplicative_widen=self.multiplicative_widen, add_noise=True)
         elif self.widen_method == 'netmorph':
-            r_2_wider_r_(self.conv1, (self.conv1.weight.data.size(0),32,32), self.W1, extra_channels=num_channels,
+            r_2_wider_r_(self.conv1, (self.conv1.weight.data.size(0),16,16), self.W1, extra_channels=num_channels,
                          init_type="match_std_exact", function_preserving=True,
                          multiplicative_widen=self.multiplicative_widen, net_morph=False)
 
