1878e6f337f3eee107995ea91562f16f6504f3e4
Michael Painter <michael@Administrators-MacBook-Pro.local>
2019-01-12 01:57:15 +0000

    Changed params

--- src/main.py
+++ src/main.py
@@ -536,7 +536,7 @@ def get_defaults(script_name):
         }
     elif script == "cifar_weight_viz_conv":
         return {
-            "lr": 3.0e-3,
+            "lr": 1.0e-2,
             "weight_decay": 1.0e-2,
             "epochs": 100,
             "tb_dir": tb_log_dir,
