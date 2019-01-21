0b438b527649b622d4adece6f777f5027009bfac
Michael Painter <michael@Administrators-MacBook-Pro.local>
2019-01-21 14:20:55 +0000

    Replaced pooling by a conv and added strides in viz

--- src/viz.py
+++ src/viz.py
@@ -132,15 +132,15 @@ class Conv_Net(nn.Module):
         super(Conv_Net, self).__init__()
         self.widen_method = widen_method.lower()
         self.multiplicative_widen = multiplicative_widen
-        self.conv1 = nn.Conv2d(in_channels, conv_channels, kernel_size=9, padding=4)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
-        self.W1 = nn.Linear(conv_channels * 16 * 16, hidden_units)
+        self.conv1 = nn.Conv2d(in_channels, conv_channels, kernel_size=9, padding=4, stride=2)
+        self.conv2 = nn.Conv2d(conv_channels, conv_channels * 2, kernel_size=3, padding=1, stride=2)
+        self.W1 = nn.Linear(conv_channels * 2 * 8 * 8, hidden_units)
         self.bn = nn.BatchNorm1d(num_features=hidden_units)
         self.W2 = nn.Linear(hidden_units, 10)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.pool1(x)
+        x = self.conv2(x)
         x = flatten(x)
         x = self.W1(x)
         x = F.relu(x)
