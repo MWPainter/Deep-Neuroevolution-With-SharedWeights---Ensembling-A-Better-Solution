f2f34c5ea72527f54465852ac48f15457e693339
michaelpainter <michaelpainter@dhcpieb219.eng.ox.ac.uk>
2019-01-25 17:29:37 +0000

    Typo in RMS optimizer

--- src/run.py
+++ src/run.py
@@ -81,7 +81,7 @@ def _make_optimizer_fn_rms(model, lr, weight_decay, args, momentum=None):
     :return: The optimizer for the network optimizer, which is passed into the remaining
         training loop functions
     """
-    return t.optim.RMSprop(model.parameters(), lr=lr, weight_decay=weight_decay, amsgrad=True, momentum=args.momentum)
+    return t.optim.RMSprop(model.parameters(), lr=lr, weight_decay=weight_decay, momentum=args.momentum)
 
 
 
